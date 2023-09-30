c5caa297123f764b636f0cde94f4671044f62832
JS <jpits@jksmith.com>
2023-09-30 12:24:13 +0100

    Added AV sync

--- server.sh
+++ server.sh
@@ -9,13 +9,14 @@ run_ffmpeg () {
 	PWD=$(echo $1 | cut -f2 -d'|')
 	ffmpeg \
                 -hide_banner -loglevel info \
-                -f alsa -thread_queue_size 8192 -copyts -sample_rate 48000 -channels 2 -channel_layout stereo -i ${AD} \
-                -f v4l2 -thread_queue_size 8192 -use_wallclock_as_timestamps 1 -copyts -framerate 60 -video_size 1280x720 -i /dev/${VD} \
-                -map 1:v \
-		-c:v h264_omx -tune zerolatency -b:v 6M -maxrate:v 8M -bufsize:v 500M -r 60 \
+                -f alsa -thread_queue_size 8192 -use_wallclock_as_timestamps 1 -sample_rate 48000 -channels 2 -channel_layout stereo -i ${AD} \
+                -itsoffset 0.3 \
+                -f v4l2 -thread_queue_size 8192 -use_wallclock_as_timestamps 1 -framerate 60 -video_size 1280x720 -i /dev/${VD} \
                 -map 0:a \
                 -c:a aac -b:a 128k -ac 2 -ar 48k \
-		-f mpegts -r 60 -start_at_zero "srt://0.0.0.0:${PORT}?password=${PWD}&mode=listener&transtype=live"
+                -map 1:v \
+                -c:v h264_omx -tune zerolatency -b:v 6M -maxrate:v 8M -bufsize:v 500M \
+		-f mpegts -fflags +genpts -start_at_zero "srt://0.0.0.0:${PORT}?password=${PWD}&mode=listener&transtype=live"
         sleep 1
 	return $?
 }
